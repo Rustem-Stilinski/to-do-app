42ce11dfa39758b11fc8c0ac518d5dee9cf56af6
Rustem Aisariyev <scorus0808@gmail.com>
2025-03-21 22:06:28 -0400

    added spring boot project

---
+++ src/test/java/com/aisariyev/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisariyev.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
